adf5bf1d774834d35f75ecb23ca574dcd2fa9ad7
leakin185 <65524912+leakin185@users.noreply.github.com>
2023-01-31 16:39:45 +0800

    modified person model on admin page display and added search function

--- demoapp/admin.py
+++ demoapp/admin.py
@@ -21,7 +21,7 @@ class NewAdmin(UserAdmin):
     readonly_fields = [ 
         'date_joined', 
     ] 
-
+    
     def get_form(self, request, obj=None, **kwargs): 
         form = super().get_form(request, obj, **kwargs) 
         is_superuser = request.user.is_superuser 
@@ -30,3 +30,8 @@ class NewAdmin(UserAdmin):
             form.base_fields['username'].disabled = True 
 
         return form 
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin): 
+    list_display = ("last_name", "first_name") 
+    search_fields = ("first_name__startswith", ) 
--- demoapp/models.py
+++ demoapp/models.py
@@ -27,3 +27,10 @@ class Reservation(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     time_log = models.TimeField(help_text="Enter the exact time!")
+
+class Person(models.Model): 
+    last_name = models.TextField() 
+    first_name = models.TextField() 
+
+    def __str__(self): 
+        return f"{self.last_name}, {self.first_name}" 
