af7b56ff2a89f32fb99da4e8d5ac123bd2d04ffd
leakin185 <65524912+leakin185@users.noreply.github.com>
2023-02-02 16:31:38 +0800

    added template for menu page

--- demoapp/views.py
+++ demoapp/views.py
@@ -39,11 +39,11 @@ def drinks(request, drink_name):
     return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
 
 def about(request): 
-    about_content = {'about':"Little Lemon is an Italian restaurant"}
+    about_content = {'about':"Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."}
     return render(request, "about.html", about_content)
 
 def menu(request): 
-    return HttpResponse("Menu")
+    return render(request, "menu.html")
 
 def book(request): 
     return HttpResponse("Make a booking")
